18f417573bc609ec978b71f61e589ab3037bcc69
nickscha <nick@scharnbeck.de>
2026-04-26 19:37:34 +0200

    [MIN] format code

--- src/win32_zai.c
+++ src/win32_zai.c
@@ -2114,8 +2114,8 @@ ZAI_API ZAI_INLINE void initialize_density_grid(f32 *grid, i32 dim, f32 world_si
       {
         f32 wx = (((f32)x / ((f32)dim - 1.0f)) - 0.5f) * world_size + chunk_coord.x;
 
-        /* f32 noise_val = zai_noise_perlin_3_fbm(wx, wy, wz, frequency, 6, lacunarity, gain); */
-        /* f32 noise_val = zai_noise_3d_fbm(wx, wy, wz, frequency, 6, lacunarity, gain, (i32)0xDEADBEEF); */
+        /* f32 noise_val = zai_noise_perlin_3_fbm(wx, wy, wz, frequency, octaves, lacunarity, gain); */
+        /* f32 noise_val = zai_noise_3d_fbm(wx, wy, wz, frequency, octaves, lacunarity, gain, seed); */
         f32 noise_val = zai_noise_3d_fbm_rotation(wx, wy, wz, frequency, octaves, lacunarity, gain, seed, zai_noise_rotation);
         f32 offset = -wy * 0.6f;
         f32 final_density = (noise_val * amplitude) + offset;
