da3806d64a0d1a4e1cc4ad7f89182176f73394cc
nickscha <nick@scharnbeck.de>
2026-06-14 12:35:29 +0200

    [FONT] add mipmap

--- src/win32_zai.c
+++ src/win32_zai.c
@@ -2134,10 +2134,11 @@ ZAI_API void zai_render_font(win32_zai_state *state, zai_camera *camera)
       glBindTexture(GL_TEXTURE_2D, tex_font);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
       glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
       glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, (i32)w, (i32)h, 0, GL_RGBA, GL_UNSIGNED_BYTE, file_contents);
+      glGenerateMipmap(GL_TEXTURE_2D);
       glActiveTexture(GL_TEXTURE0);
 
       VirtualFree(file_contents, 0, MEM_RELEASE);
