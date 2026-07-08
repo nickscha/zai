a8d0329c8b09835c2cda7c021184de754f679696
nickscha <nick@scharnbeck.de>
2026-07-08 07:50:39 +0200

    [CORE] geometry prototype

---
+++ src/zai_geometry.h
@@ -0,0 +1,11 @@
+#ifndef ZAI_GEOMETRY_H
+#define ZAI_GEOMETRY_H
+
+#include "zai_types.h"
+
+/* #############################################################################
+ * # [SECTION] Geometry Generation Functions
+ * #############################################################################
+ */
+
+#endif /* ZAI_GEOMETRY_H */
