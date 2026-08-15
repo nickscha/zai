ea655130f6e1ae9f137c9f939a186437b1bf2988
nickscha <nick@scharnbeck.de>
2026-08-15 04:14:48 +0200

    [TILE] added cloud shadow visualization

--- zai_tiles.fs
+++ zai_tiles.fs
@@ -241,5 +241,7 @@ void main()
         FragColor = vec4(normal * 0.5 + 0.5, 1.0); /* map normal to 0 - 1 range */
     }  else if (visualization_mode < 2.5f) {
         FragColor = v_is_dirty_pass > 0.5 ? vec4(1.0, 0.1, 0.1, 1.0) : vec4(col, 1.0);
+    }  else if (visualization_mode < 3.5f) {
+        FragColor = vec4(cloudShadow, cloudShadow, cloudShadow, 1.0);
     }
 }
